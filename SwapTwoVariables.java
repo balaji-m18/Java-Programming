730c1853ced8ebcfcbc094f7ac68c713317cbd05
Balaji <balajim180904@gmail.com>
2025-01-23 09:15:05 +0530

    Add files via upload

---
+++ SwapTwoVariables.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class SwapTwoVariables {
+	public static void main(String[] args){
+		int x = 10;
+		int y = 20;
+		int temp;
+		
+		temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
---
+++ Variables.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Variables {
+	public static void main(String[] args) {
+		int x = 10;
+		double y = 15.45;
+		String z = "Hello";
+		boolean a = true;
+		char b = 'A';
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
